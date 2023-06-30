bf7447ab16a204a7af0a77e5a35748d274c3167e
oberonlai <m615926@gmail.com>
2023-06-30 12:29:42 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,94 @@
+# WP Updater v1.0
+
+> Simple WordPress Class for self host plugin updating server from [How to Configure Self-Hosted Updates for Your Private Plugins](https://rudrastyh.com/wordpress/self-hosted-plugin-update.html) 
+
+## Requirements
+
+* PHP >=7.2
+* [Composer](https://getcomposer.org/)
+* [WordPress](https://wordpress.org) >=5.4
+
+## Installation
+
+#### Install with composer
+
+Run the following in your terminal to install with [Composer](https://getcomposer.org/).
+
+```
+$ composer require oberonlai/wp-updater
+```
+
+WP Metabox [PSR-4](https://www.php-fig.org/psr/psr-4/) autoloading and can be used with the Composer's autoloader. Below is a basic example of getting started, though your setup may be different depending on how you are using Composer.
+
+```php
+require __DIR__ . '/vendor/autoload.php';
+
+use ODS\Updater;
+
+$options = array( ... );
+
+$books = new Updater( $options );
+
+```
+
+See Composer's [basic usage](https://getcomposer.org/doc/01-basic-usage.md#autoloading) guide for details on working with Composer and autoloading.
+
+## Basic Usage
+
+Below is a basic example of setting up a plugin updater.
+
+```php
+// Require the Composer autoloader.
+require __DIR__ . '/vendor/autoload.php';
+
+// Import PostTypes.
+use ODS\Updater;
+
+```
+
+## Usage
+
+To create a updater, first instantiate an instance of `Updater`. The class takes one argument, which is an associative array.
+
+```php
+$updater = new Updater( array(
+	'plugin_slug' => 'my-plugin',
+	'version'     => '1.0.0',
+	'json_url'    => 'https://mydomain.com/my-plugin.json',
+));
+```
+
+## Preparations
+You will need a hosting space to store the information file of the plugin and the zip file of the new version of the plugin. You can use space such as Dropbox or Google Drive, or you can place it on your own server. My top recommendation is to host it on GitHub, as it allows integration with version control processes.
+
+## my-plugin.json
+
+This is an example of a JSON-formatted file for the plugin information. It includes the following fields:
+
+```JSON
+{
+	"name": "My Plugin",
+	"slug": "my-plugin",
+	"author": "<a href='https://mydomain.com/my-plugin/'>Author</a>",
+	"author_profile": "https://mydomain.com",
+	"version": "1.0.1",
+	"download_url": "https://mydomain.com/my-plugin.zip",
+	"requires": "5.6",
+	"tested": "6.2.2",
+	"requires_php": "7.4",
+	"added": "2021-04-05 00:00:00",
+	"last_updated": "2023-06-29 00:00:00",
+	"homepage": "https://mydoamin.com",
+	"sections": {
+	  "description": "My plugin description",
+	  "installation": "My plugin installation",
+	  "changelog": "<p>v1.0.1</p><p>Change log</p>"
+	},
+	"banners": {
+	  "low": "https://mydomain.com/my-plugin-banner-low.jpg",
+	  "high": "https://mydomain.com/my-plugin-banner-high.jpg"
+	}
+}
+```
+
+If you want to push a notification for a new update, simply increment the version number in the plugin. The download_url indicates the location to download the new version of the plugin.
---
+++ src/Updater.php
@@ -0,0 +1,207 @@
+<?php
+
+namespace ODS;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Updater plugin class inspired by Misha Rudrastyh
+ * https://rudrastyh.com/wordpress/self-hosted-plugin-update.html
+ */
+class Updater {
+
+	/**
+	 * Plugin slug
+	 *
+	 * @var string $plugin_slug Plugin slug.
+	 */
+	private $plugin_slug;
+
+	/**
+	 * Current theme or plugin version
+	 *
+	 * @var string $version Current theme or plugin version.
+	 */
+	private $version;
+
+	/**
+	 * Cache key
+	 *
+	 * @var string $cache_key Cache key.
+	 */
+	private $cache_key;
+
+	/**
+	 * Cache allowed
+	 *
+	 * @var bool $cache_allowed Cache allowed.
+	 */
+	private $cache_allowed;
+
+	/**
+	 * JSON URL
+	 *
+	 * @var string $json_url JSON URL for plugin information.
+	 */
+	private $json_url;
+
+	/**
+	 * Construct
+	 *
+	 * @param array $args Arguments.
+	 *
+	 * @return void
+	 */
+	public function __construct( $args ) {
+
+		$this->plugin_slug   = $args['plugin_slug'];
+		$this->version       = $args['version'];
+		$this->cache_key     = $args['plugin_slug'] . '_updater';
+		$this->$json_url     = $args['json_url'];
+		$this->cache_allowed = false;
+
+		add_filter( 'plugins_api', array( $this, 'info' ), 20, 3 );
+		add_filter( 'site_transient_update_plugins', array( $this, 'update' ) );
+		add_action( 'upgrader_process_complete', array( $this, 'purge' ), 10, 2 );
+
+	}
+
+	/**
+	 * The information on update popup
+	 *
+	 * @param object $response Response.
+	 * @param string $action Action.
+	 * @param object $args Arguments.
+	 *
+	 * @return object $response Response.
+	 */
+	public function info( $response, $action, $args ) {
+
+		if ( 'plugin_information' !== $action ) {
+			return $response;
+		}
+
+		if ( empty( $args->slug ) || $this->plugin_slug !== $args->slug ) {
+			return $response;
+		}
+
+		$remote = $this->request();
+
+		if ( ! $remote ) {
+			return $response;
+		}
+
+		$response = new \stdClass();
+
+		$response->name           = $remote->name;
+		$response->slug           = $remote->slug;
+		$response->version        = $remote->version;
+		$response->tested         = $remote->tested;
+		$response->requires       = $remote->requires;
+		$response->author         = $remote->author;
+		$response->author_profile = $remote->author_profile;
+		$response->homepage       = $remote->homepage;
+		$response->download_link  = $remote->download_url;
+		$response->trunk          = $remote->download_url;
+		$response->requires_php   = $remote->requires_php;
+		$response->last_updated   = $remote->last_updated;
+
+		$response->sections = array(
+			'description'  => $remote->sections->description,
+			'installation' => $remote->sections->installation,
+			'changelog'    => $remote->sections->changelog,
+		);
+
+		if ( ! empty( $remote->banners ) ) {
+			$response->banners = array(
+				'low'  => $remote->banners->low,
+				'high' => $remote->banners->high,
+			);
+		}
+
+		return $response;
+
+	}
+
+	/**
+	 * Get the remote json data
+	 *
+	 * @return object $remote Remote Json data.
+	 */
+	public function request() {
+
+		$remote = get_transient( $this->cache_key );
+
+		if ( false === $remote || ! $this->cache_allowed ) {
+
+			$remote = wp_remote_get(
+				$this->json_url,
+				array(
+					'timeout' => 60,
+					'headers' => array(
+						'Accept' => 'application/json',
+					),
+				)
+			);
+
+			if ( is_wp_error( $remote ) || 200 !== wp_remote_retrieve_response_code( $remote ) || empty( wp_remote_retrieve_body( $remote ) ) ) {
+				return false;
+			}
+
+			set_transient( $this->cache_key, $remote, DAY_IN_SECONDS );
+
+		}
+
+		$remote = json_decode( wp_remote_retrieve_body( $remote ) );
+
+		return $remote;
+
+	}
+
+	/**
+	 * Update
+	 *
+	 * @param object $transient Transient.
+	 *
+	 * @return object $transient Transient.
+	 */
+	public function update( $transient ) {
+
+		if ( empty( $transient->last_checked ) ) {
+			return $transient;
+		}
+
+		$remote = $this->request();
+
+		if ( $remote && version_compare( $this->version, $remote->version, '<' ) && version_compare( $remote->requires, get_bloginfo( 'version' ), '<=' ) && version_compare( $remote->requires_php, PHP_VERSION, '<' ) ) {
+			$response              = new \stdClass();
+			$response->slug        = $this->plugin_slug;
+			$response->plugin      = "{$this->plugin_slug}/{$this->plugin_slug}.php";
+			$response->new_version = $remote->version;
+			$response->tested      = $remote->tested;
+			$response->package     = $remote->download_url;
+
+			$transient->response[ $response->plugin ] = $response;
+
+		}
+
+		return $transient;
+
+	}
+
+	/**
+	 * Purge cache
+	 *
+	 * @param object $upgrader Upgrader.
+	 * @param array  $options Options.
+	 *
+	 * @return void
+	 */
+	public function purge( $upgrader, $options ) {
+		if ( $this->cache_allowed && 'update' === $options['action'] && 'plugin' === $options['type'] ) {
+			// just clean the cache when new plugin version is installed.
+			delete_transient( $this->cache_key );
+			do_action( 'ods_updater_after_purge', $upgrader, $options );
+		}
+	}
+}
